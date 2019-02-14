9bffd03dee5473c434f2c81e78bf850bfbf18666
haniokasai <htek@haniokasai.com>
2019-02-15 00:21:31 +0900

    鳥羽完成

--- cpumultilimit.py
+++ cpumultilimit.py
@@ -1,9 +1,12 @@
 # coding: UTF-8
 import subprocess
 import threading
+import time
+
 from subprocess import check_output
 
 #######config###########
+
 
 limitpercent = 30
 limitprocessname ="bedrock_server"
@@ -30,24 +33,30 @@ def scraiping_pid(processname):
  except Exception:
   print("scraiping_pidException")
 
-def checkschedule(pid):
-
-
 ########################
 
 
 ########Main Thread#####
 
 pidsmap = scraiping_pid(limitprocessname)
+print("pidsmap:"+str(pidsmap))
 for p in pidsmap:
  thread_1 = threading.Thread(target=cd_exec(p))
 
 while True:
  newpid = scraiping_pid(limitprocessname)
+ print("pidsmap:" + str(newpid))
  #差分で回す　newpid,pidsmap
- thread_1 = threading.Thread(target=cd_exec(p))
+ #https://xwave.exblog.jp/11309038/
+ set_ab = set(newpid) - set(pidsmap)
+ list_ab = list(set_ab)
+ print("list_ab:" + str(list_ab))
+ for l in list_ab:
+  thread_2 = threading.Thread(target=cd_exec(l))
+
  pidsmap = newpid
- 
+ time.sleep(60)
+
 
 ########################
 
